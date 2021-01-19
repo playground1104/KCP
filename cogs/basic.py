cfdf86f434471277c31eb9b9edea216c8b2a3434
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-01-19 22:11:39 +0900

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -144,6 +144,125 @@ class Basic(commands.Cog):
             embed.colour = 0xff0000
         return await ctx.send(embed=embed)
 
+    @commands.command(name="뭉치검수")
+    async def check_craft(self, ctx: commands.Context):
+        msg = ctx.message
+        ans = "```부품 수, 금지부품, 장갑두께, 트윅스케일, 무장점수, 크기\n"
+        if len(ctx.message.attachments) == 0 or not ctx.message.attachments[0].filename.endswith(".craft"):
+            try:
+                msg = await self.bot.wait_for("message", timeout=30,
+                                              check=lambda m: m.author.id == ctx.author.id and (
+                                                          (len(m.attachments) != 0) or (m.content == "!뭉치검수")))
+                if msg.content == "!뭉치검수":
+                    return await ctx.send(ans + "```")
+            except asyncio.TimeoutError:
+                return await ctx.send(ans + "```")
+        for craft in [x for x in msg.attachments if x.filename.endswith(".craft")]:
+            craft_content = (await craft.read()).decode("UTF-8")
+
+            ap = 0.0
+
+            c = kspconfig.loadl(craft_content.split('\n'))
+
+            part_list = []
+
+            error_blacklist = set()
+            error_armorthickness = dict()
+            error_tweak = dict()
+
+            for d in c["PART"]:
+                e = '_'.join(d["part"].split("_")[:-1])
+                part_list.append(e)
+                try:
+                    f = self.partdb[e]
+                except KeyError:
+                    error_blacklist.add(e)
+                    continue
+                ap += f['point']
+                g = 0
+                try:
+                    m = d["MODULE"]
+                except KeyError:
+                    m = []
+                for h in m:
+                    if h["name"] == "HitpointTracker":
+                        g = int(h["Armor"])
+                    elif h["name"] == "TweakScale":
+                        topt = f['tweakoption']
+                        cs = float(h["currentScale"])
+                        ds = float(h["defaultScale"])
+                        if cs > ds and not ("u" in topt):
+                            error_tweak[e] = [cs, "u"]
+                        elif cs < ds and not ("d" in topt):
+                            error_tweak[e] = [cs, "d"]
+
+                t = f['armorthickness']
+                if g > t:
+                    error_armorthickness[e] = [g, t]
+
+            size_split = c["size"].split(',')
+            size_width = float(size_split[0])
+            size_height = float(size_split[1])
+            size_length = float(size_split[2])
+            berror_size = (size_width > 19.0005) or (size_height > 8.0005) or (size_length > 20.0005)
+            berror_ap = ap > 17.1
+            berror_blacklist = False
+            berror_armorthickness = False
+            berror_tweak = False
+            berror_partcount = len(part_list) > 250
+            ans += f"{c['ship']:20}"
+            if berror_partcount:
+                ans += f"🔴"
+            else:
+                ans += f"🟢"
+
+            if len(error_blacklist) > 0:
+                ans += f"🔴"
+                berror_blacklist = True
+            else:
+                ans += f"🟢"
+            if len(error_armorthickness) > 0:
+                s = ""
+                for k, v in error_blacklist:
+                    if len(s) != 0:
+                        s = s + ", "
+                    s = s + k + ": " + str(v[0]) + " > " + str(v[1])
+                ans += f"🔴"
+                berror_armorthickness = True
+            else:
+                ans += f"🟢"
+
+            if len(error_tweak) > 0:
+                s = ""
+                for k, v in error_tweak:
+                    if len(s) != 0:
+                        s = s + ", "
+                    s = s + k + ": " + str(v[0])
+                    if v[1] == "u":
+                        s = s + " UT"
+                    elif v[1] == "d":
+                        s = s + " DT"
+                ans += f"🔴"
+                berror_tweak = True
+            else:
+                ans += f"🟢"
+
+            if berror_ap:
+                ans += f"🔴"
+            else:
+                ans += f"🟢"
+
+            if berror_size:
+                ans += f"🔴"
+            else:
+                ans += f"🟢"
+
+            if not (
+                    berror_ap or berror_armorthickness or berror_blacklist or berror_size or berror_tweak or berror_partcount):
+                ans += f"  🔴\n"
+            else:
+                ans += f"  🟢\n"
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Basic(bot))
